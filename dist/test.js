a85b4a62864b9903a5819870dca39a93ed801f12
Sharafat Rohoman <mdsharafatrohomanzafourllah@example.com>
2026-05-07 19:17:33 +0600

    first commit

---
+++ dist/test.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// A simple TypeScript program
+console.log("Hello, TypeScript!");
+let string = "This is a string variable.";
+console.log(string);
+//# sourceMappingURL=test.js.map
---
+++ src/test.ts
@@ -0,0 +1,8 @@
+
+
+// A simple TypeScript program
+console.log("Hello, TypeScript!");
+
+
+let string:string = "This is a string variable.";
+console.log(string);
